2dfc72a3c4fb543281a0606ff5824d6d01588566
Litchi017 <lizhihappyday@126.com>
2015-04-01 15:45:15 -0600

    update test.py

--- test.py
+++ test.py
@@ -7,7 +7,23 @@ class TestStringMethods(unittest.TestCase):
 	  good = calculation.calculation()
 	  self.assertTrue(good.findFood("chicken")) 
 	  #function findFood return true if food in database otherwise return false
-
+	  return good
+  def test_in_database(self):
+	  good = calculation.calculation()
+	  self.asserTrue(good.findFood("chicken"))
+	  return good
+  def test_food_one(self):
+	  good = calculation.calculation()
+	  self.assertTrue(good.findFood("chicken"))
+	  return good
+  def test_food_two(self):
+	  good = calculation.calculation()
+	  self.assertTrue(good.findFood("chicken"))
+	  return good
+  def test_food_three(self):
+	  good = calculation.calculation()
+	  self.assertTrue(good.findFood("chicken"))
+	  return good
 
 if __name__ == '__main__':
     unittest.main()
