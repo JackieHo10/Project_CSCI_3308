50e764286d7017be2e369b5fd19ba45af52ae1bd
Malaokia <mili0392@colorado.edu>
2015-04-02 11:35:09 -0600

    php's files for testing homework

---
+++ signup.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title>Test for Course Listing</title>
+</head>
+<body>
+	<?php
+		
+		$dbhost="localhost";
+		$dbpassword="Yzz6QBv6PpzeTYY3";
+		$dbuser="mql";
+		$conn = new mysqli($dbhost,$dbuser,$dbpassword);
+		if($conn->connect_error)
+		{
+			die ("connection error".$conn->error);
+		}
+		echo "connected<br>";
+
+		/*$sl = "CREATE DATABASE DB_CALCOUNTER";
+		if (!$conn->select_db("DB_CALCOUNTER"))
+		{
+			$conn->query($sl);
+			$conn->select_db("DB_CALCOUNTER)");
+			echo "new database DB_CALCOUNTER<br>";
+		}
+		else
+		{
+			$conn->select_db("DB_CALCOUNTER");
+			echo "has database already! DB_CALCOUNTER<br>";
+		}*/
+		$conn->select_db("DB_CALCOUNTER");
+		
+		$tbname = "USERLOGIN";
+		$username = $_POST["username"];
+
+		$userpas  = $_POST["abc"];
+		$usermail = $_POST["email"];
+		$secureq  = $_POST["securityq"];
+		$securea  = $_POST["securitya"];
+		echo "$username<br>";
+		echo "$userpas<br>";
+		echo "$usermail<br>";
+		echo "$secureq<br>";
+		echo "$securea<br>";
+		/*
+		if(!($conn->query($tbname)))
+		{
+			$sl = "CREATE TABLE USERLOGIN(".
+				  "LOGINID INT NOT NULL AUTO_INCREMENT, ".
+				  "LOGINNAME VARCHAR(25) NOT NULL, ".
+				  "LOGINPASS VARCHAR(25) NOT NULL, ".
+				  "LOGINMAIL VARCHAR(25) NOT NULL, ".
+				  "SECUREQ VARCHAR(20) NOT NULL, ".
+				  "SECUREA VARCHAR(10) NOT NULL, ".
+				  "PRIMARY KEY(LOGINID));";
+			$result = $conn->query($sl);
+			if(!$result)
+			{
+				die("<br> error creating ".$conn->error);
+			}
+			echo "<br>succes";
+		}
+		*/
+		if(isset($username,$userpas,$usermail,$secureq,$securea))
+		{
+			$sql = "INSERT INTO USERLOGIN ".
+				   "(loginname,loginpass,loginmail,secureq,securea) ".
+				   "VALUES ".
+				   "('$username','$userpas','$usermail','$secureq','$securea')";
+			$result=$conn->query($sql);
+			if(!$result)
+			{
+				die("error inserting".$conn->error);
+			}
+			echo "successfully inserting";
+			$result->free($sql);
+		}
+		$conn->close();
+	?>
+</body>
+</html>
