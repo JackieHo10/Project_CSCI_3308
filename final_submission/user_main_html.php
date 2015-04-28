408a340e53ee141c7cd559f6607e73204a6423c7
Malaokia <mili0392@colorado.edu>
2015-04-27 18:14:23 -0600

    some changes

--- final_submission/user_main_html.php
+++ final_submission/user_main_html.php
@@ -1,45 +1,42 @@
-
-
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
-     <head>
-     	
-	<!-- this a main page for user after login into our website-->
-		
-		<script>
-		$(document).ready(function(){
-			document.body.style.background = " url(icecreamball.jpg) no-repeat center";
-		});
-		function logout(){
-			var x;
-			if (confirm("Do You Want to Logout")){
-				window.location.replace("login_html.php");
-			}
-			else {
-				window.location.replace("user_main_html.php");
-			}
-			
-		}
-	</script>
-		<center>
-			<p><font size="35" color="blue"><?php
-			session_start();
-			$var_value = $_SESSION['username'];
-			echo "Welcome {$var_value}";
-			?></font></p>
-			<div style="height:30px"></div>
-			<p><font face="Geneva, Arial, Helvetica" size="105" color="brown">CalCounter</font></p>
-			<!-- Put Ray API in here   -->
-			<center>
-				<script src="http://platform.fatsecret.com/js?key=8e6d9c349428497d93b054d131d67bb7&auto_load=true"></script>
+<head>
+<title>CalCounter</title>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js">
+</script>
+<!--- set the background-->
+<script>
+$(document).ready(function(){
+    document.body.style.background = "#CED8F6 url('icecreamball.jpg') center no-repeat";
+    });
+</script>
 
-				<!--<div style="height:200px"></div>
-				<input style="width:380px; height:30px" type="text" name="food bar" value= "" placeholder="Enter food here Eg.chicken soup, salad,..." id="location" onKeydown="if (event.keyCode==13){ print(input)};" ></input> 
-				<div style="height:-100px"></div>-->
-			<center>
-			
-			<div style="height:150px"></div>		
-			<button color="black" size="45" style="height=30px;width=500px" onclick="logout();">LOGOUT</button>
-		<center>
+</head>
+<!--- script below will relocate to login page when clicked-->
+<script>
+function loginpage(){
+  window.location.replace("http://localhost/Project_CalorieTracker/login_html.php");
+}
+function signuppage(){
+  window.location.replace("http://localhost/Project_CalorieTracker/signup_html.php");
+}
+</script>
+<center>
+<div style="height:20px"></div>
+<div style="position: aboslute; top:0;right:40%;width:50%;text-align:left;">
+</div><font face="Geneva, Arial, Helvetica,sans-serif" size="80" color="brown">CalCounter</font>
+<div style="height:20px"></div>
+<p><button style="height: 30px; width: 500px" color="black" size="45" onclick="signuppage();">Sign Up !</button></p>   
+<button style="height: 30px; width: 500px" color="black" size="45" onclick="loginpage();" >User Login !</button>
+<p><font size = "4.5" color="white"></font></p>
+<font size="4"><p id="weather"> </p></font>
+<div style="height:20px"></div>
+</center>
+</body>
+<center>
+<video width="500" height="200" controls>
+<source src="CalCounter.mp4" type="video/mp4">
+</video>
+</center>
 </html>
 <div style="position: aboslute; top:0;right:100%;width:100%;text-align:right;"><font color="red" size="5">#PZMRJ-Production</font></div>
